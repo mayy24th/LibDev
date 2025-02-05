1c43cf24015a2e32a34c23e69457e0cc58585528
youngchive <102659915+youngchive@users.noreply.github.com>
2025-02-05 16:43:42 +0900

    init commit

---
+++ src/main/java/com/example/LibDev/LibDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibDevApplication.class, args);
+	}
+
+}
