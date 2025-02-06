637c1e7259124e751e077cb10e4be9620d6141c4
youngchive <102659915+youngchive@users.noreply.github.com>
2025-02-06 20:42:00 +0900

    Feat: 도서 등록

--- src/main/java/com/example/LibDev/book/controller/BookAPIController.java
+++ src/main/java/com/example/LibDev/book/controller/BookAPIController.java
@@ -1,4 +1,21 @@
 package com.example.LibDev.book.controller;
 
+import com.example.LibDev.book.dto.BookRequestDto;
+import com.example.LibDev.book.entity.Book;
+import com.example.LibDev.book.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/books")
+@RequiredArgsConstructor
 public class BookAPIController {
+    private final BookService bookService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Book> createBook(@RequestBody BookRequestDto bookRequestDto) {
+        Book savedBook = bookService.saveBook(bookRequestDto);
+        return ResponseEntity.ok(savedBook);
+    }
 }
--- src/main/java/com/example/LibDev/book/service/BookService.java
+++ src/main/java/com/example/LibDev/book/service/BookService.java
@@ -1,4 +1,21 @@
 package com.example.LibDev.book.service;
 
+import com.example.LibDev.book.dto.BookRequestDto;
+import com.example.LibDev.book.dto.BookResponseDto;
+import com.example.LibDev.book.entity.Book;
+import com.example.LibDev.book.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class BookService {
+    private final BookRepository bookRepository;
+
+    @Transactional
+    public Book saveBook(BookRequestDto bookRequestDto) {
+        Book book = bookRequestDto.toEntity();
+        return bookRepository.save(book);
+    }
 }
