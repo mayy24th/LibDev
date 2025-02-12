feca2bc61267193e67ddde10ce34649ec575528c
jeongseokhyeon <jeong9235@gmail.com>
2025-02-12 18:51:48 +0900

    Feat: 이메일 중복 에러코드 추가

--- src/main/java/com/example/LibDev/global/exception/CustomErrorCode.java
+++ src/main/java/com/example/LibDev/global/exception/CustomErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum CustomErrorCode {
     //user 관련 에러
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다"),
+    DUPLICATE_EMAIL(HttpStatus.CONFLICT,"이미 사용 중인 이메일입니다"),
 
     // borrow 관련 에러
     BORROW_NOT_FOUND(HttpStatus.NOT_FOUND, "대출 내역을 찾을 수 없습니다"),
