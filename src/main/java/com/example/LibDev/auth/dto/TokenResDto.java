1fd926f509c7dd5ef6c12bcba7c3d625147bf49a
jeongseokhyeon <jeong9235@gmail.com>
2025-02-14 14:35:58 +0900

    Feat: AccessToken 생성, RefreshToken 생성 로직 분리

--- src/main/java/com/example/LibDev/auth/dto/TokenResDto.java
+++ src/main/java/com/example/LibDev/auth/dto/TokenResDto.java
@@ -1,14 +1,14 @@
 package com.example.LibDev.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
+@AllArgsConstructor
+@Builder
 @Getter
 public class TokenResDto {
     private final String accessToken;
     private final String refreshToken;
 
-    public TokenResDto(String accessToken, String refreshToken) {
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-    }
 }
