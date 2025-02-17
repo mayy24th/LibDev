c7e4dc8f720f6ce1dd25b224e36d21ca25cb3188
jeongseokhyeon <jeong9235@gmail.com>
2025-02-17 20:54:41 +0900

    Chore: 카멜케이스 적용

--- src/main/java/com/example/LibDev/user/entity/User.java
+++ src/main/java/com/example/LibDev/user/entity/User.java
@@ -27,10 +27,10 @@ public class User extends BaseEntity {
     private String phone;
 
     /*대출가능상태*/
-    private boolean borrow_available;
+    private boolean borrowAvailable;
 
     /*패널티만료일*/
-    private LocalDateTime penalty_expiration;
+    private LocalDateTime penaltyExpiration;
 
     /*탈퇴여부*/
     private boolean withdraw;
@@ -47,6 +47,6 @@ public class User extends BaseEntity {
 
     public void updatePassword(String password) {this.password = password;}
 
-    public void updateBorrowAvailable(Boolean borrow_available) {this.borrow_available = borrow_available;}
+    public void updateBorrowAvailable(Boolean borrowAvailable) {this.borrowAvailable = borrowAvailable;}
 
 }
