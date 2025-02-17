c0c7ac1de24d6a98f6d445937121ca4bc291a15b
mayy24th <86235986+mayy24th@users.noreply.github.com>
2025-02-17 20:53:40 +0900

    Feat: 리뷰 커스텀 에러코드 추가

--- src/main/java/com/example/LibDev/global/exception/CustomErrorCode.java
+++ src/main/java/com/example/LibDev/global/exception/CustomErrorCode.java
@@ -10,6 +10,7 @@ public enum CustomErrorCode {
     //user 관련 에러
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다"),
     DUPLICATE_EMAIL(HttpStatus.CONFLICT,"이미 사용 중인 이메일입니다"),
+    AUTHENTICATION_REQUIRED(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다."),
 
     // borrow 관련 에러
     BORROW_NOT_FOUND(HttpStatus.NOT_FOUND, "대출 내역을 찾을 수 없습니다"),
@@ -22,7 +23,11 @@ public enum CustomErrorCode {
     USER_RESERVATION_FULL(HttpStatus.BAD_REQUEST, "사용자는 최대 5권까지만 예약할 수 있습니다."),
     BOOK_ALREADY_RESERVED(HttpStatus.BAD_REQUEST, "이미 해당 책을 예약하셨습니다."),
     RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "예약 정보를 찾을 수 없습니다."),
-    RESERVATION_CANCELLATION_FORBIDDEN(HttpStatus.FORBIDDEN, "본인의 예약만 취소할 수 있습니다.");
+    RESERVATION_CANCELLATION_FORBIDDEN(HttpStatus.FORBIDDEN, "본인의 예약만 취소할 수 있습니다."),
+
+    // review 관련 에러
+    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "한줄평 정보를 찾을 수 없습니다."),
+    REVIEW_DELETE_FORBIDDEN(HttpStatus.BAD_REQUEST, "본인이 작성한 한줄평만 삭제할 수 있습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
