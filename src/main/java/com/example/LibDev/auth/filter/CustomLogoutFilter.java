c7648890d6dbf157437fb4ad21cd13f969da4fbf
jeongseokhyeon <jeong9235@gmail.com>
2025-02-18 15:13:56 +0900

    Chore: 사용되지않는 import 삭제

--- src/main/java/com/example/LibDev/auth/filter/CustomLogoutFilter.java
+++ src/main/java/com/example/LibDev/auth/filter/CustomLogoutFilter.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.filter.GenericFilterBean;
--- src/main/java/com/example/LibDev/auth/jwt/JwtFilter.java
+++ src/main/java/com/example/LibDev/auth/jwt/JwtFilter.java
@@ -5,7 +5,6 @@ import com.example.LibDev.global.dto.GlobalResponseDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
