71ff16e402e824f6bbd31b29dc5ff17a64e59229
mayy24th <86235986+mayy24th@users.noreply.github.com>
2025-02-19 16:15:44 +0900

    Feat: 유사도서 조회 쿼리 분리에 따른 매퍼 메서드 분리

--- src/main/java/com/example/LibDev/recommendation/mapper/RecommendationBookMapper.java
+++ src/main/java/com/example/LibDev/recommendation/mapper/RecommendationBookMapper.java
@@ -7,10 +7,17 @@ import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface RecommendationBookMapper {
-    /** 도서 상세 페이지 유사 도서 추천 **/
-    List<RecommendationResponseDto> findSimilarBooks(@Param("bookId") Long bookId,
-                                                     @Param("topicId") Integer topicId,
-                                                     @Param("author") String author);
+    /** 도서 상세 페이지 해당 도서와 유사한 추천 (보고있는 도서는 제외) **/
+    // 동일한 주제 (대출 횟수, 최신 출판)
+    List<RecommendationResponseDto> findBooksByTopic(@Param("excludedBooks") List<Long> excludedBooks,
+                                                     @Param("topicId") Integer topicId);
+    // 동일한 저자 (대출 횟수, 최신 출판)
+    List<RecommendationResponseDto> findBooksByAuthor(@Param("excludedBooks") List<Long> excludedBooks,
+                                                      @Param("author") String author,
+                                                      @Param("limit") int limit);
+    // 주제, 저자와 상관없이 최신 출판 도서 추가
+    List<RecommendationResponseDto> findLatestPublishedBooks(@Param("excludedBooks") List<Long> excludedBooks,
+                                                             @Param("limit") int limit);
 
     /** 메인페이지 유저 대출내역 기반 도서 추천 **/
     List<RecommendationResponseDto> findUserBaseBooks(@Param("userId") Long userId,
