bd23b472a7242221b20f71a11d1c9b6de9b221fe
mayy24th <86235986+mayy24th@users.noreply.github.com>
2025-02-19 15:06:44 +0900

    Feat: 인기도서 추천 중복 쿼리 통합

--- src/main/java/com/example/LibDev/recommendation/mapper/PopularBookMapper.java
+++ src/main/java/com/example/LibDev/recommendation/mapper/PopularBookMapper.java
@@ -8,9 +8,7 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface PopularBookMapper {
     /** 메인페이지 비로그인, 대출정보 없는 유저 도서 추천 **/
-    List<RecommendationResponseDto> findPopularBooks();
-
     /** 대출내역 기반으로 추천 후에 인기도서로 부족한 도서데이터 추천 **/
-    List<RecommendationResponseDto> findPopularBooksExcluding(@Param("excludedBooks") List<Long> excludedBooks,
-                                                              @Param("limit") int limit);
+    List<RecommendationResponseDto> findPopularBooks(@Param("excludedBooks") List<Long> excludedBooks,
+                                                     @Param("limit") int limit);
 }
