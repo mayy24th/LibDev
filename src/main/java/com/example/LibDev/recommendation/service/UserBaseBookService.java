af94574e751f608c700f9e55f3ff485733c11fce
mayy24th <86235986+mayy24th@users.noreply.github.com>
2025-02-19 11:21:52 +0900

    Feat: 유저기반 도서 추천 서비스코드 추가

---
+++ src/main/java/com/example/LibDev/recommendation/service/UserBaseBookService.java
@@ -0,0 +1,66 @@
+package com.example.LibDev.recommendation.service;
+
+import com.example.LibDev.book.entity.Book;
+import com.example.LibDev.borrow.entity.Borrow;
+import com.example.LibDev.borrow.repository.BorrowRepository;
+import com.example.LibDev.recommendation.dto.RecommendationResponseDto;
+import com.example.LibDev.recommendation.mapper.RecommendationMapper;
+import com.example.LibDev.user.dto.UserResDto;
+import com.example.LibDev.user.entity.User;
+import com.example.LibDev.user.repository.UserRepository;
+import com.example.LibDev.user.service.UserService;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserBaseBookService {
+
+    private final RecommendationMapper recommendationMapper;
+    private final BorrowRepository borrowRepository;
+    private final UserRepository userRepository;
+    private final UserService userService;
+
+    private static final int RECOMMENDATION_LIMIT = 5;
+
+    /**
+     *  메인페이지에서 사용자 기반 도서 추천
+     *  - 비로그인
+     *  - popularBooks
+     *
+     *  - 로그인
+     *  - 사용자의 대출내역이 존재하지 않는 처음에는 인기도서 (대출 수 기반)
+     *  - 사용자의 대출내역이 존재하는 경우 대출횟수가 가장 높은 도서를 기반으로 조회
+     */
+    public List<RecommendationResponseDto> findUserBaseBooks(){
+        UserResDto userResDto = userService.info();
+        if(userResDto == null) {
+            return recommendationMapper.findPopularBooks();
+        }
+
+        User user = userRepository.findByEmail(userResDto.getEmail())
+                .orElseThrow(()-> new IllegalArgumentException("회원 정보를 찾을 수 없습니다."));
+
+        boolean hasBorrowHistory = borrowRepository.existsByUser(user);
+        if (!hasBorrowHistory) {
+            return recommendationMapper.findPopularBooks();
+        }
+
+        Integer mostBorrowedTopic = recommendationMapper.findMostBorrowedTopic(user.getId());
+        List<RecommendationResponseDto> recommendations = recommendationMapper.findUserBaseBooks(user.getId(), mostBorrowedTopic);
+        if(recommendations.size() < RECOMMENDATION_LIMIT){
+            List<Long> excludedBookIds = recommendations.stream()
+                    .map(RecommendationResponseDto::getBookId)
+                    .toList();
+
+            List<RecommendationResponseDto> popularBooks = excludedBookIds.isEmpty()
+                    ? recommendationMapper.findPopularBooks()
+                    : recommendationMapper.findPopularBooksExcluding(excludedBookIds, RECOMMENDATION_LIMIT - recommendations.size());
+
+            recommendations.addAll(popularBooks);
+        }
+        return recommendations;
+    }
+}
