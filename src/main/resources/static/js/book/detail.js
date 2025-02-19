ea93e5424803d093aae704b96c71990fee0ec9af
youngchive <102659915+youngchive@users.noreply.github.com>
2025-02-19 20:39:15 +0900

    Feat: 도서 상세페이지 구현

---
+++ src/main/resources/static/js/book/detail.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const bookInfo = document.querySelector(".book-info");
+    if (!bookInfo) {
+        alert("도서 정보를 로드할 수 없습니다.");
+    }
+});
