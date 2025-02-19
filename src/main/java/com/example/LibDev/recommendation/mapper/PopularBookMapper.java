072f61c7569ec8f981f682615a9ff16979c7de29
mayy24th <86235986+mayy24th@users.noreply.github.com>
2025-02-19 13:10:37 +0900

    Feat: 도서 추천 MyBatis 인터페이스 기능 단위로 분리

---
+++ src/main/java/com/example/LibDev/recommendation/mapper/PopularBookMapper.java
@@ -0,0 +1,16 @@
+package com.example.LibDev.recommendation.mapper;
+
+import com.example.LibDev.recommendation.dto.RecommendationResponseDto;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface PopularBookMapper {
+    /** 메인페이지 비로그인, 대출정보 없는 유저 도서 추천 **/
+    List<RecommendationResponseDto> findPopularBooks();
+
+    /** 대출내역 기반으로 추천 후에 인기도서로 부족한 도서데이터 추천 **/
+    List<RecommendationResponseDto> findPopularBooksExcluding(@Param("excludedBooks") List<Long> excludedBooks,
+                                                              @Param("limit") int limit);
+}
--- src/main/java/com/example/LibDev/recommendation/mapper/RecommendationMapper.java
+++ src/main/java/com/example/LibDev/recommendation/mapper/RecommendationMapper.java
@@ -12,14 +12,7 @@ public interface RecommendationMapper {
                                                      @Param("topicId") Integer topicId,
                                                      @Param("author") String author);
 
-    /** 메인페이지 비로그인, 대출정보 없는 유저 도서 추천 **/
-    List<RecommendationResponseDto> findPopularBooks();
-
     /** 메인페이지 유저 대출내역 기반 도서 추천 **/
-    Integer findMostBorrowedTopic(@Param("userId") Long userId);
     List<RecommendationResponseDto> findUserBaseBooks(@Param("userId") Long userId,
                                                       @Param("topicId") Integer topicId);
-    List<RecommendationResponseDto> findPopularBooksExcluding(@Param("excludedBooks") List<Long> excludedBooks,
-                                                              @Param("limit") int limit);
-
 }
