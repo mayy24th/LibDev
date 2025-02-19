ea1a45b626740d9a695d8e078ff74518dc637cf5
mayy24th <86235986+mayy24th@users.noreply.github.com>
2025-02-19 12:04:40 +0900

    Feat: 대출내역 기반으로 추천 시 부족한 데이터 추가하는 메서드 분리

--- src/main/java/com/example/LibDev/recommendation/service/UserBaseBookService.java
+++ src/main/java/com/example/LibDev/recommendation/service/UserBaseBookService.java
@@ -1,16 +1,14 @@
 package com.example.LibDev.recommendation.service;
 
-import com.example.LibDev.book.entity.Book;
-import com.example.LibDev.borrow.entity.Borrow;
 import com.example.LibDev.borrow.repository.BorrowRepository;
+import com.example.LibDev.global.exception.CustomErrorCode;
+import com.example.LibDev.global.exception.CustomException;
 import com.example.LibDev.recommendation.dto.RecommendationResponseDto;
 import com.example.LibDev.recommendation.mapper.RecommendationMapper;
-import com.example.LibDev.user.dto.UserResDto;
 import com.example.LibDev.user.entity.User;
 import com.example.LibDev.user.repository.UserRepository;
 import com.example.LibDev.user.service.UserService;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -25,37 +23,31 @@ public class UserBaseBookService {
 
     private static final int RECOMMENDATION_LIMIT = 5;
 
-    /**
-     *  메인페이지에서 사용자 기반 도서 추천
-     *  - 비로그인
-     *  - popularBooks
-     *
-     *  - 로그인
-     *  - 사용자의 대출내역이 존재하지 않는 처음에는 인기도서 (대출 수 기반)
-     *  - 사용자의 대출내역이 존재하는 경우 대출횟수가 가장 높은 도서를 기반으로 조회
-     */
+    /** 메인페이지에서 유저 기반 도서 추천 **/
     public List<RecommendationResponseDto> findUserBaseBooks(){
-
-        // 비로그인
+        /** 비로그인 - popularBooks **/
         String email = userService.getUserEmail();
         if(email == null){
             return recommendationMapper.findPopularBooks();
         }
 
-        // 로그인
+        /** 로그인 **/
         User user = userRepository.findByEmail(email)
-                .orElseThrow(()-> new IllegalArgumentException("회원 정보를 찾을 수 없습니다."));
+                .orElseThrow(()-> new CustomException(CustomErrorCode.USER_NOT_FOUND));
 
-        // 대출내역이 존재하지 않는 경우
-        boolean hasBorrowHistory = borrowRepository.existsByUser(user);
-        if (!hasBorrowHistory) {
-            return recommendationMapper.findPopularBooks();
-        }
+        // 대출내역이 존재하지 않는 경우 경우 - 인기도서 (대출 수 기반)
+        if (!borrowRepository.existsByUser(user)) {return recommendationMapper.findPopularBooks();}
 
-        // 대출내역이 존재하는 경우
+        // 대출내역이 존재하는 경우 - 최다, 최신 대출 도서를 기반으로 조회
         Integer mostBorrowedTopic = recommendationMapper.findMostBorrowedTopic(user.getId());
         List<RecommendationResponseDto> recommendations = recommendationMapper.findUserBaseBooks(user.getId(), mostBorrowedTopic);
-        if(recommendations.size() < RECOMMENDATION_LIMIT){
+        fillWithPopularBooks(recommendations);
+
+        return recommendations;
+    }
+
+    private void fillWithPopularBooks(List<RecommendationResponseDto> recommendations) {
+        if (recommendations.size() < RECOMMENDATION_LIMIT) {
             List<Long> excludedBookIds = recommendations.stream()
                     .map(RecommendationResponseDto::getBookId)
                     .toList();
@@ -66,6 +58,5 @@ public class UserBaseBookService {
 
             recommendations.addAll(popularBooks);
         }
-        return recommendations;
     }
 }
