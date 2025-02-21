7073f6052812b3aa5542b9e4d63190e4f681d90d
jeongseokhyeon <jeong9235@gmail.com>
2025-02-21 16:35:27 +0900

    Fix: 무한 미로그인 알림 수정

--- src/main/resources/static/js/utils/reissue.js
+++ src/main/resources/static/js/utils/reissue.js
@@ -11,9 +11,7 @@ export async function reissue() {
 
 
     } catch (error){
-        console.log("로그인되지 않음");
-        alert("로그인이 필요합니다.");
-        window.location.href = "/users/login"; // 로그인 페이지로 이동
-        return null; // 로그인하지 않은 경우 null 반환
+        console.log("로그인되어 있지 않음");
+        return null;
     }
 }
