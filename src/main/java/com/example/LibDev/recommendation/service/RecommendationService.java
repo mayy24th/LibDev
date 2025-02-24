96d2a2644d5ea541005fdc99377205283ca6c316
mayy24th <86235986+mayy24th@users.noreply.github.com>
2025-02-24 15:09:55 +0900

    Feat: 인기도서 조회 서비스 코드 추가

--- src/main/java/com/example/LibDev/recommendation/service/RecommendationService.java
+++ src/main/java/com/example/LibDev/recommendation/service/RecommendationService.java
@@ -1,6 +1,7 @@
 package com.example.LibDev.recommendation.service;
 
 import com.example.LibDev.recommendation.dto.RecommendationResponseDto;
+import com.example.LibDev.recommendation.strategy.PopularBookRecommendation;
 import com.example.LibDev.recommendation.strategy.SimilarBookRecommendation;
 import com.example.LibDev.recommendation.strategy.UserBaseRecommendation;
 import com.example.LibDev.user.service.UserService;
@@ -16,6 +17,7 @@ public class RecommendationService {
 
     private final SimilarBookRecommendation similarBookRecommendation;
     private final UserBaseRecommendation userBaseRecommendation;
+    private final PopularBookRecommendation popularBookRecommendation;
     private final UserService userService;
     private final RecommendationCacheService recommendationCacheService;
 
@@ -26,6 +28,7 @@ public class RecommendationService {
      */
     public List<RecommendationResponseDto> recommendSimilarBooks(Long bookId) {
         String cacheKey = "similar_books:" + bookId;
+
         List<RecommendationResponseDto> cachedRecommendations = recommendationCacheService.getCachedRecommendations(cacheKey);
         if (cachedRecommendations != null && !cachedRecommendations.isEmpty()) {
             return cachedRecommendations;
@@ -44,12 +47,29 @@ public class RecommendationService {
         String email = userService.getUserEmail();
         String cacheKey = "user_base_books:" + email;
 
-        List<RecommendationResponseDto> cachedRecommendations = recommendationCacheService.getCachedRecommendations(cacheKey); // ✅ Redis에서 캐시 조회
+        List<RecommendationResponseDto> cachedRecommendations = recommendationCacheService.getCachedRecommendations(cacheKey);
         if (cachedRecommendations != null) {
             return cachedRecommendations;
         }
 
         List<RecommendationResponseDto> recommendations = userBaseRecommendation.recommend(null, email);
+        recommendationCacheService.cacheRecommendations(cacheKey, recommendations);
+        return recommendations;
+    }
+
+    /**
+     * 전체 대출수를 기반으로 인기 도서 추천
+     * @return 추천 도서 리스트
+     */
+    public List<RecommendationResponseDto> recommendPopularBooks() {
+        String cacheKey = "popular_books";
+
+        List<RecommendationResponseDto> cachedRecommendations = recommendationCacheService.getCachedRecommendations(cacheKey);
+        if (cachedRecommendations != null) {
+            return cachedRecommendations;
+        }
+
+        List<RecommendationResponseDto> recommendations = popularBookRecommendation.recommend(null, null);
         recommendationCacheService.cacheRecommendations(cacheKey, recommendations);
         return recommendations;
     }
