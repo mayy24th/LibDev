72a465f3b3ea606b4539ef868ff98b2cbb5c8edd
jeongseokhyeon <jeong9235@gmail.com>
2025-02-24 15:21:15 +0900

    Chore: 사용되지 않은 import 문 삭제

--- src/main/java/com/example/LibDev/auth/service/AuthService.java
+++ src/main/java/com/example/LibDev/auth/service/AuthService.java
@@ -11,7 +11,6 @@ import com.example.LibDev.user.entity.User;
 import com.example.LibDev.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
