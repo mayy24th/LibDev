d7d3011f8f3fa2e3857060b765f7e691be6156e0
mayy24th <86235986+mayy24th@users.noreply.github.com>
2025-02-24 12:29:05 +0900

    Feat: 홈 컨트롤러 추가

---
+++ src/main/java/com/example/LibDev/global/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.LibDev.global.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    /** 전체 한줄평 조회 **/
+    @GetMapping
+    public String home() {
+        return "home/home";
+    }
+}
