6b74298420192b2b7d4f9e974afa1b92250ef650
gaeunnnnn <gaeunn02@naver.com>
2025-02-25 15:01:09 +0900

    Design: confirmToast 적용

--- src/main/resources/static/js/borrow/approveReturn.js
+++ src/main/resources/static/js/borrow/approveReturn.js
@@ -1,27 +1,26 @@
 import { formatDate, statusColor } from "./utils.js";
 import { showAlertToast } from "../utils/showAlertToast.js";
+import { showConfirmToast } from "../utils/showConfirmToast.js";
 
 export async function approveReturn(borrowId) {
-    if (!confirm("해당 도서를 반납 처리 하시겠습니까?")) {
-        return;
-    }
+    showConfirmToast("해당 도서를 반납 처리 하시겠습니까?", async () => {
+        try {
+            const response = await fetch(`/api/v1/approve-return/${borrowId}`, {
+                method: "PATCH"
+            });
+            const data = await response.json();
 
-    try {
-        const response = await fetch(`/api/v1/approve-return/${borrowId}`, {
-            method: "PATCH"
-        });
-        const data = await response.json();
+            if (!response.ok) {
+                showAlertToast(data.message);
+                return;
+            }
 
-        if (!response.ok) {
-            showAlertToast(data.message);
-            return;
+            updateReturnStatus(data);
+            showAlertToast("해당 도서가 반납 처리 되었습니다.");
+        } catch (error) {
+            console.error("Error:", error);
         }
-
-        updateReturnStatus(data);
-        showAlertToast("해당 도서가 반납 처리 되었습니다.");
-    } catch (error) {
-        console.error("Error:", error);
-    }
+    });
 }
 
 function updateReturnStatus(borrow) {
