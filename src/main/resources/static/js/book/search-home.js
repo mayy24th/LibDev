de4dc724a70e83005c7f6db06d1208d6cbd02870
youngchive <102659915+youngchive@users.noreply.github.com>
2025-02-26 06:00:44 +0900

    Feat: 이미지 슬라이드 구현

--- src/main/resources/static/js/book/search-home.js
+++ src/main/resources/static/js/book/search-home.js
@@ -7,4 +7,17 @@ document.addEventListener("DOMContentLoaded", function() {
             window.location.href = `/books/search-simple?query=${encodeURIComponent(searchQuery)}&searchType=${encodeURIComponent(searchType)}`;
         }
     });
+
+    const backgroundContainer = document.querySelector('.background-container');
+    const images = ['/images/libImage1.jpg', '/images/libImage2.jpg', '/images/libImage3.jpg']; // 추가된 이미지 경로
+    let currentImageIndex = 0;
+
+    backgroundContainer.style.backgroundImage = `url('${images[currentImageIndex]}')`;
+
+    function showNextImage() {
+        currentImageIndex = (currentImageIndex + 1) % images.length; // 다음 이미지 인덱스 계산
+        backgroundContainer.style.backgroundImage = `url('${images[currentImageIndex]}')`; // 배경 이미지 변경
+    }
+
+    setInterval(showNextImage, 3000);
 });
