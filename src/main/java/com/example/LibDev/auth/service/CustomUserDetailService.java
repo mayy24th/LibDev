014db486cceabbf4a7565cba55bf799e186b6c43
jeongseokhyeon <jeong9235@gmail.com>
2025-02-26 04:47:22 +0900

    Chore: 아이디 혹은 비밀번호가 틀렸을 시 알림이 발생하도록 수정

--- src/main/java/com/example/LibDev/auth/service/CustomUserDetailService.java
+++ src/main/java/com/example/LibDev/auth/service/CustomUserDetailService.java
@@ -1,8 +1,6 @@
 package com.example.LibDev.auth.service;
 
 import com.example.LibDev.auth.dto.CustomUserDetails;
-import com.example.LibDev.global.exception.CustomErrorCode;
-import com.example.LibDev.global.exception.CustomException;
 import com.example.LibDev.user.entity.User;
 import com.example.LibDev.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +17,7 @@ public class CustomUserDetailService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new CustomException(CustomErrorCode.USER_NOT_FOUND));
+                .orElseThrow(() -> new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다.:"+ email));
 
         if(!user.isWithdraw()){
             return new CustomUserDetails(user);
